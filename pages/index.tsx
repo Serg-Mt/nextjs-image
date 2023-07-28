3b018ff6dad6b0668b17b070308680cb79e5cd01
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2023-07-28 17:23:48 +0400

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image';
+import largePic from '../public/stephen-leonardi-iSM8Z82uxn8-unsplash.jpg';
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+  return <>
+
+    <Image
+      src={largePic}
+      alt='Mountain landscape'
+      placeholder="blur"
+      style={{objectFit: 'cover'}}
+      fill={true}
+    />
+    <div className='center'><h1>Example</h1></div>
+    
+  </>;
+
 }
